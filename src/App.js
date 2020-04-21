775a40fbb6e164627889a3f357da7791b980d2ed
Oleg Pukhaev <nuba8184@gmail.com>
2020-04-21 09:16:44 +0600

    Add login page html

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import "./App.css";
 function App() {
   return (
     <div className="mainContainer">
-      <h1>Autorization page here</h1>
+      <header className="mainHeader">
+        <h1>Header</h1>
+      </header>
+      <section className="mainSection">
+        <section className="mainSection__login">
+          <div className="loginForm">
+            <h2>Login:</h2>
+            <form>
+              <input type="text" placeholder="User" />
+              <input type="password" placeholder="Password" />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </section>
+        {/* <section className="mainSection__register"></section> */}
+        {/* <section className="mainSection__profile"></section> */}
+      </section>
     </div>
   );
 }
