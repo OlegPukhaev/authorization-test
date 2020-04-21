89bc58d8a24d944b5b37697cf45a4518e0036b65
Oleg Pukhaev <nuba8184@gmail.com>
2020-04-21 09:52:38 +0600

    Add register page and profile page html, css

--- src/App.js
+++ src/App.js
@@ -9,17 +9,48 @@ function App() {
       </header>
       <section className="mainSection">
         <section className="mainSection__login">
-          <div className="loginForm">
+          <div className="formContainer">
             <h2>Login:</h2>
             <form>
-              <input type="text" placeholder="User" />
+              <input type="email" placeholder="E-mail" />
               <input type="password" placeholder="Password" />
               <button type="submit">Login</button>
             </form>
           </div>
         </section>
-        {/* <section className="mainSection__register"></section> */}
-        {/* <section className="mainSection__profile"></section> */}
+        <section className="mainSection__register">
+          <div className="formContainer">
+            <h2>Login:</h2>
+            <form>
+              <input type="email" placeholder="E-mail" />
+              <input type="text" placeholder="Name" />
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Confirm password" />
+              <button type="submit">Login</button>
+            </form>
+          </div>
+        </section>
+        <section className="mainSection__profile">
+          <h1>Profile page:</h1>
+          <div className="fakeImage"></div>
+          <article>
+            <ul className="profile">
+              <li>
+                <span>Name:</span> Oleg
+              </li>
+              <li>
+                <span>Email:</span> nuba8184@gmail.com
+              </li>
+              <li>
+                <span>Info:</span>
+                <p>
+                  Человек который зарегистрировался и работает за деньги. И ищет
+                  сксесс в своей жизни
+                </p>
+              </li>
+            </ul>
+          </article>
+        </section>
       </section>
     </div>
   );
