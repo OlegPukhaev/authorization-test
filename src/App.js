bde177d0370f7e9bd061a4789f4471cdbfa025ae
Oleg Pukhaev <nuba8184@gmail.com>
2020-04-21 11:04:02 +0600

    Implement react router navigation

--- src/App.js
+++ src/App.js
@@ -3,26 +3,49 @@ import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Profile from "./components/Profile";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="mainContainer">
-      <header className="mainHeader">
-        <h1>Header</h1>
-        <nav className="mainNavigation">
-          <ul className="mainNavigation_items">
-            <li className="mainNavigation_item">Login</li>
-            <li className="mainNavigation_item">Register</li>
-            <li className="mainNavigation_item">Profile</li>
-          </ul>
-        </nav>
-      </header>
-      <section className="mainSection">
-        <Login />
-        <Register />
-        <Profile />
-      </section>
-    </div>
+    <Router>
+      <div className="mainContainer">
+        <header className="mainHeader">
+          <h1>Header</h1>
+          <nav className="mainNavigation">
+            <ul className="mainNavigation_items">
+              <li className="mainNavigation_item">
+                <Link className="linkStyle" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="mainNavigation_item">
+                <Link className="linkStyle" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li className="mainNavigation_item">
+                <Link className="linkStyle" to="/profile">
+                  Profile
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section className="mainSection">
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </section>
+      </div>
+    </Router>
   );
 }
 
