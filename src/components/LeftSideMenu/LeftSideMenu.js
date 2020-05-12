0c2fb88e3b21df59d12ff112cf641dae2127757b
Oleg Pukhaevm <olegpukhaev@gmail.com>
2020-05-12 11:35:06 +0600

    Redirect and 404 pages with redirect ro products

--- src/components/LeftSideMenu/LeftSideMenu.js
+++ src/components/LeftSideMenu/LeftSideMenu.js
@@ -1,17 +1,35 @@
 import React from "react";
 import "./style.css";
 import MenyListItems from "../MenuListItems";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 const LeftSideMenu = () => {
   return (
     <div className="container">
-      <ul className="menuList">
-        <MenyListItems name="Гитары" />
-        <MenyListItems name="Казаны" />
-        <MenyListItems name="Афганские казаны" />
-        <MenyListItems name="Шуруповерты" />
-        <MenyListItems name="Палатки" />
-      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/products" />
+        </Route>
+        <Route exact path="/products">
+          <ul className="menuList">
+            <MenyListItems name="Гитары" />
+            <MenyListItems name="Казаны" />
+            <MenyListItems name="Афганские казаны" />
+            <MenyListItems name="Шуруповерты" />
+            <MenyListItems name="Палатки" />
+          </ul>
+        </Route>
+        <Route exact path="/finances">
+          <ul className="menuList">
+            <MenyListItems name="Отчет" />
+            <MenyListItems name="Прогноз" />
+            <MenyListItems name="Баланс" />
+          </ul>
+        </Route>
+        <Route path="*">
+          <Redirect to="/products" />
+        </Route>
+      </Switch>
     </div>
   );
 };
